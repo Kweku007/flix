067cb45a8c501ad249f14ed0e32444b3d2f0483c
Kweku007 <kna38@drexel.edu>
2020-10-02 15:19:08 -0400

    Part 2: Added tab bar navigation and collections view

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Kweku Aboagye on 10/2/20.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet var posterView: UIImageView!
+    
+    
+    
+    
+    
+    
+}
