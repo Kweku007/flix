10f2fe511f7b642195af3db30e373c66f0a5461e
Kweku007 <kna38@drexel.edu>
2020-10-19 22:07:29 -0400

    Auto Layout

--- flix/MovieGridViewController.swift
+++ flix/MovieGridViewController.swift
@@ -41,7 +41,7 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
               print(error.localizedDescription)
            } else if let data = data {
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            
+    
             
                 self.movies = dataDictionary["results"] as! [[String : Any]]
                 self.collectionView.reloadData()
